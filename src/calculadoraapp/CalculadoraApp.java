2225058d7a044d1a056d875732f6db2c22e8288a
itzcoatl397 <itzcoatl3b@gmail.com>
2025-06-26 23:35:40 -0600

    first commit

---
+++ src/calculadoraapp/CalculadoraApp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadoraapp;
+
+import calculadoraapp.view.Window;
+
+/**
+ *
+ * @author Gerar
+ */
+public class CalculadoraApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Window window = new Window();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+    }
+    
+}
